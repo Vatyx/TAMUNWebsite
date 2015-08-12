09b376a72961b363549d2b5651b6f99e3340c091
Vatyx <sahildhanju1@gmail.com>
2015-08-12 01:32:27 -0500

    Adding base of website for others to see

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$('#back').hide();
+$('#topLogoWhite').hide();
+//setInterval(function () {$('#back').fadeOut();}, 3000);
+
+$('#top').parallax({imageSrc: 'images/smallerbackground.png'});
+$('#second-top').parallax({imageSrc: 'images/topbackground.png'});
+$('#third-top').parallax({imageSrc: 'images/scrollingbackground2fixed.jpg'});
+
+var startY = $('.navbar').height() * 2;
+var hasChanged = false;
+function checkScroll(){
+     //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+	    if(!hasChanged)
+	    {   
+	        $('.navbar').addClass("scrolled");
+	        $('#topLogoBlack').fadeOut(200);
+	        $('#topLogoWhite').fadeIn(200);
+
+	        $('#navList li a').each(function() {
+	        	$(this).css('color', 'white');
+	        });
+			
+			hasChanged = true;
+		}
+    	}else{
+	        $('.navbar').removeClass("scrolled");
+	        $('#topLogoWhite').fadeOut(200);
+	        $('#topLogoBlack').fadeIn(200);
+
+	        $('#navList li a').each(function() {
+	        	$(this).css('color', 'black');
+	        });
+	        hasChanged = false;
+    	}
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+// var jumboHeight = $('#top').outerHeight();
+// function parallax(){
+//     var scrolled = $(window).scrollTop();
+//     console.log(scrolled);
+//     $('#top').css('height', (jumboHeight-scrolled) + 'px');
+// }
+
+// $(window).scroll(function(e){
+//     parallax();
+//     console.log("test");
+// });
